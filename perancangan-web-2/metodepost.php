9b19e5ee68ea53c4f562bf51a695e559ee3e5c42
M.Usman Khfdz <niihanii142@gmail.com>
2025-10-28 11:07:40 +0700

    Create metodepost.php

---
+++ perancangan-web-2/metodepost.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Form method POST</title>
+    </head>
+    <body>
+        <h1>Input</h1>
+        <form action="metodepostproses.php" method="post">
+            Masukkan nama: 
+            <input type="text" name="nama" size="25">
+            <input type="submit" value="Proses">
+        </form>
+    </body>
+</html>
