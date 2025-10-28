4a3245583ab641123a70c1949cc385b34d67c1f3
M.Usman Khfdz <niihanii142@gmail.com>
2025-10-28 11:09:31 +0700

    Create metodepostproses.php

---
+++ perancangan-web-2/metodepostproses.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Method POST Proses</title>
+    </head>
+    <body>
+        Data nama yang diinputkan adalah :
+        <?php echo $_POST["nama"]; ?>
+    </body>
+</html>
