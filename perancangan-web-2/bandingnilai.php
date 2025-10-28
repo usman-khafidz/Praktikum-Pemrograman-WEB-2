c9d12cdbf8b133db6cd424c0756a4f9f44df446e
M.Usman Khfdz <niihanii142@gmail.com>
2025-10-28 11:07:17 +0700

    Create bandingnilai.php

---
+++ perancangan-web-2/bandingnilai.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Contoh Form dengan POST</title>
+    </head>
+    <body>
+        <form action="proc_post.php" method="post">
+            Bil I :
+            <input type="text" name="bil1">
+            <br>
+            Bil II:
+            <input type="text" name="bil2">
+            <br>
+            <input type="submit" value="Bandingkan">
+        </form>
+    </body></html>
