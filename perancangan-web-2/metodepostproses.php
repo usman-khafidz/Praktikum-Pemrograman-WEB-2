0cb38a16a3889bab0b0ff9ca7dbdb79c80c84707
M.Usman Khfdz <niihanii142@gmail.com>
2025-10-28 11:17:51 +0700

    Delete metodepostproses.php

--- perancangan-web-2/metodepostproses.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>Method POST Proses</title>
-    </head>
-    <body>
-        Data nama yang diinputkan adalah :
-        <?php echo $_POST["nama"]; ?>
-    </body>
-</html>
