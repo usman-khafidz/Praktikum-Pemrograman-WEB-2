e3eb883d265bb799caeb9d63fef4adb1149c5c0f
M.Usman Khfdz <niihanii142@gmail.com>
2025-10-28 11:09:41 +0700

    Create proc_login (1).php

---
+++ perancangan-web-2/proc_login (1).php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Proses Input</title>
+    </head>
+    <body>
+        <?
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+        ?>
+        Username : <?echo $username?>
+        <br>
+        Password : <?echo $password?>
+        <br>
+    </body>
+    </html>
