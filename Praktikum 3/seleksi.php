72ebe72d7d84bae2e51ea0651f36b53bec1a14de
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:16 +0700

    Create seleksi.php

---
+++ Praktikum 3/seleksi.php
@@ -0,0 +1,14 @@
+<html lang="en">
+    <head>
+        <title>Seleksi</title>
+    </head>
+    <body>
+        <?php
+        $a = 10;
+        $b = 5;
+        if ($a > $b) {
+            echo 'a lebih besar dari b';
+        }
+        ?>
+        </body>
+        </html>
