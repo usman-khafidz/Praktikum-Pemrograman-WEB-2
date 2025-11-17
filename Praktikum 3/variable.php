464c5ae8cc5fb71d3a7eac3c076cfbbb20455799
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:30 +0700

    Create variable.php

---
+++ Praktikum 3/variable.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo Variabel</title>
+    </head>
+    <body>
+        <?php
+        $bil = 3;
+        echo $bil;
+        ?>
+        </body>
+        </html>
