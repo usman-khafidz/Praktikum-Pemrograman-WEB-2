1d42d98e716967ed8d0581366c3468f8b42a0990
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:13 +0700

    Create php_html.php

---
+++ Praktikum 3/php_html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo</title>
+    </head>
+    <body>
+        <?php
+        echo 'Kode PHP di sini';
+        ?>
+        <p> Dokumen HTML </p>
+        <?php
+        echo 'Kode PHP di sini';
+        ?>
+        </body>
+        </html>
