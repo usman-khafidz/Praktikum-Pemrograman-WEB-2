85212e768e72faa1eec6745067a462455c39aa1f
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:09 +0700

    Create pengulangan_while.php

---
+++ Praktikum 3/pengulangan_while.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Loop while</title>
+    </head>
+    <body>
+        <?php
+        $i = 0;
+        while ($i < 10) {
+            echo $i;
+            $i++;
+        }
+        ?>
+        </body>
+        </html>
