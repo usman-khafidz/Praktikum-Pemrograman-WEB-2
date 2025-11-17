c7639add5e8258de89d283d3b95f2f28d4b6f497
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:01 +0700

    Create isset.php

---
+++ Praktikum 3/isset.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo Variabel</title>
+    </head>
+    <body>
+        <?php
+        $bil = 3;
+        var_dump($bil);
+        $var = "";
+        var_dump($var);
+        $var = null;
+        var_dump($var);
+        ?>
+        </body>
+        </html>
