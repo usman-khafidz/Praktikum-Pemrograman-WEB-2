7392c6a5bba45335dea3411ff9533f6e3876bf3e
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:05 +0700

    Create pengulangan_dowhiile.php

---
+++ Praktikum 3/pengulangan_dowhiile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Loop do-while</title>
+    </head>
+    <body>
+        <?php
+        $i = 0;
+        do {
+            echo $i;
+            $i++;
+        } while ($i < 10);
+        ?>
+        </body>
+        </html>
