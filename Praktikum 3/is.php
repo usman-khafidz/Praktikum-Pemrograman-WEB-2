7aa2fbb51582caf7ec84f4e017b51da1126fa758
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:48:52 +0700

    Create is.php

---
+++ Praktikum 3/is.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Cek Tipe</title>
+    </head>
+    <body>
+        <?php
+        $bil = 3;
+        var_dump(is_int($bil));
+        $var = "";
+        var_dump(is_string($var));
+        ?>
+        </body>
+        </html>
