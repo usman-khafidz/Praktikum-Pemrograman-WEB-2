8a208e7065e376bfcad4662eb65c0d6d47580610
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:49:25 +0700

    Create var_dump.php

---
+++ Praktikum 3/var_dump.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo Variabel</title>
+    </head>
+    
+    <body>
+        <?php
+        $bil = 3;
+        var_dump($bil);
+        print_r($bil);
+        ?>
+        </body>
+        </html>
