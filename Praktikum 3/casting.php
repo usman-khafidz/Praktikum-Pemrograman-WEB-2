419baefb6247036f69b58e30d6eed4d783bc227b
M.Usman Khfdz <niihanii142@gmail.com>
2025-11-17 21:48:28 +0700

    Create casting.php

---
+++ Praktikum 3/casting.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head
+    <title>Casting Tipe</title>
+</head>
+<body>
+    <?php
+    $str = '123abc';
+    $bil = (int) $str;
+    echo gettype($str);
+    echo gettype($bil);
+    ?>
+    </body>
+    </html>
